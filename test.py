9ddb23aaf1e93a558751a85e6ad0fb78729e931e
Yash Rane <yash_rane@umail.ucsb.edu>
2018-03-02 13:05:20 -0800

    Created function that calculates weights

--- test.py
+++ test.py
@@ -10,7 +10,57 @@ import numpy as np
 import seaborn as sns
 
 
-df = pd.read_csv("approval_polllist.csv") #data frame
+approval = pd.read_csv("approval_polllist.csv") #data frame
+#sns_plot = sns.pairplot(df, size=2.5, plot_kws = {'alpha':0.1})
+#sns_plot.savefig('test.png')
 
-sns_plot = sns.pairplot(df, size=2.5, plot_kws = {'alpha':0.1})
-sns_plot.savefig('test.png')
+
+
+df = pd.read_csv('SampleSurvey.csv')
+df = df.drop([0,1],axis='index').reset_index()#drops the first two rows
+
+true_proportions = {
+        'Status': {'Survey Preview':0.5, 'IP Address':0.5},
+        'DistributionChannel': {'preview':0.3, 'anonymous':0.7}
+        }
+
+
+
+
+def createWeights(df, true_proportions):
+    '''Calculate the weights needed to make the distribution of a dataframe match the true proportio
+    Args:
+        df (dataframe): dataframe of the data you want weighted
+        true_proportions (dict): json-like dictionary of the proportions that df should match
+    Returns:
+        Series: the column of weights that that apply to each row in df
+    Raises:
+        ValueError: if `true_proportions` is not a dictionary
+    '''
+    
+    #makes a dataframe of the true demographics
+    if type(true_proportions) is type(dict()):
+        demographics = df.from_dict(true_proportions)
+    else:
+        raise TypeError('true_proportions must be of type dict')
+        
+    
+    #finds the propotions of demographics inside the sample data
+    sample = df[demographics.columns]
+    proportions = sample.apply(lambda x: x.value_counts() / len(sample) )
+    
+    #creates a function that can find the weights needed for a specified row and column
+    weights = demographics/proportions
+    calculate_weight = lambda row,colname: weights[colname][row[colname]]
+    
+    #calculates the weights for all rows
+    result = pd.Series(np.ones(len(df)))
+    for colname in demographics.columns:
+        result = result*df.apply(calculate_weight, axis=1, args=(colname,))
+    
+    return result
+
+
+
+
+df['Weight'] = createWeights(df, true_proportions)
